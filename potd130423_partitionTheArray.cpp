b928273167db3393281ea81eeeeafdc2dd401d4c
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-04-14 00:03:57 +0530

    Update and rename potd130423.cpp to potd130423_partitionTheArray.cpp

--- potd130423_partitionTheArray.cpp
+++ potd130423_partitionTheArray.cpp
@@ -1,3 +1,33 @@
+//Partition The Array
+
+/*Given an array A[] of N integers. The task is to partition the array into four non-empty contiguous subarrays P, Q, R, and S such that each element of the array A[] should be present in any subarray.
+Let W, X, Y, and Z be the sum of the elements in P, Q, R, and S respectively.
+Find the smallest absolute difference between the maximum and the minimum among W, X, Y, and Z.
+
+Example 1:
+
+Input:
+N = 5
+A[] = [4,2,2,5,1]
+Output: 4
+Explanation: let partition the array 
+P,Q,R,S = [4],[2,2],[5],[1]
+W = 4, X = 4, Y = 5, Z = 1 
+Differnce = max(W,X,Y,Z)-min(W,X,Y,Z)= 5-1 = 4 
+Example 2:
+
+Input:
+N = 4
+A[] = {4,4,4,4}
+Output: 0
+Explanation: 
+There is only one way to partition 
+the array. P,Q,R,S = [4],[4],[4],[4]*/
+
+
+
+//Answer:
+
 int binarySearch(long long arr[],int low,int high,long long sum,long long temp){
         int mid,index=-1;
         long long first,second,diff=INT_MAX;
