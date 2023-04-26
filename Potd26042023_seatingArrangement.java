f85ae5a5e2e1e6d645b520296af4b8b57db7ebf2
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-04-26 21:22:46 +0530

    Seating Arrangement Question, Easy Level
    in Java

---
+++ Potd26042023_seatingArrangement.java
@@ -0,0 +1,78 @@
+// Seating Arrangement
+// In Java
+
+/*
+You are given an integer n, denoting the number of people who needs to be seated, and a list of m integers seats, where 0 represents a vacant seat and 1 represents an already occupied seat.
+
+Find whether all n people can find a seat, provided that no two people can sit next to each other.
+
+Example 1:
+
+Input:
+n = 2
+m = 7
+seats[] = {0, 0, 1, 0, 0, 0, 1}
+Output:
+Yes
+Explanation:
+The two people can sit at index 0 and 4.
+Example 2:
+
+Input:
+n = 1
+m = 3
+seats[] = {0, 1, 0}
+Output:
+No
+Explanation:
+There is no way to get a seat for one person.
+*/
+
+
+//Answer:
+
+
+class Solution {
+    public static boolean is_possible_to_get_seats(int n, int m, int[] seats) {
+        // code here
+        
+        if(n==0)return true;
+        if(m==1)
+        {
+        if(n==1 && seats[0]==0)return true;
+        return false;
+        }
+        int count=0;
+        
+        for(int i=0;i<m;i++)
+        {
+        if(seats[i]==0){
+            if(i==0){
+                if(seats[i+1]==0){
+                    count++;
+                    seats[i]=1;
+                }
+            }
+            else if(i==m-1)
+            {
+                if(seats[i-1]==0)
+                {
+                    count++;
+                    seats[i]=1;
+                }
+            }
+            else
+            {
+              if(seats[i-1]==0 && seats[i+1] ==0)
+              {
+                  count++;
+                  seats[i] =1;
+              }
+            }
+        }
+        }
+        return count>=n;
+    
+        
+    }
+}
