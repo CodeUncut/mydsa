23681082da763f7a15f56cd15bed4807eb09ce19
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-06-18 17:27:38 +0530

    Create 180623gfg_ticketCounterAnotherWayy.java

---
+++ 180623gfg_ticketCounterAnotherWayy.java
@@ -0,0 +1,20 @@
+//User function Template for Java
+class Solution {
+    public static int distributeTicket(int N,int K)
+    {
+      int step=N/K;
+        int left= step/2;
+        int right =left;
+        
+        if(step%2==1)left++;
+        int i = K*left;
+        int j = N+1-K*right;
+        
+        if(step%2 ==1){
+            if(N%K==0)return i;
+            return i+1;
+        }
+        if(N%K==0)return j;
+        return j-1;  
+    }
+}
