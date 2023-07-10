e99682e4b5c067ef98d83c9d52eac2576e91b0c1
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-07-10 08:24:31 +0530

    Create 100723_TransposeOfMatrix.cpp
    Easy Day10 July

---
+++ 100723_TransposeOfMatrix.cpp
@@ -0,0 +1,16 @@
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        // code here 
+        for(int i=0;i<n;i++){
+         for(int j=i+1;j<n;j++){
+             int trans =matrix[i][j];
+             matrix[i][j] = matrix[j][i];
+             matrix[j][i]=trans;
+         }   
+        }
+    }
+};
