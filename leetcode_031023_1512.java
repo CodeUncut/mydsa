185225fd3f4109fe83517c2a8c860d6bf19a41eb
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-10-03 22:35:20 +0530

    Create leetcode_031023_1512.java
    easy, Number of Good Pairs

---
+++ leetcode_031023_1512.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int ans = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        for (int value : count.values()) {
+            ans += (value * (value - 1)) / 2;
+        }
+
+        return ans;
+    }
+}
