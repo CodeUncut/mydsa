fbd4f10ca6b3bc5420b66ae71df78cd55b5335ee
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-10-28 23:52:32 +0530

    Create gfgPotd_281023.java
    Bleak Number

---
+++ gfgPotd_281023.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int is_bleak(int n)
+    {
+        // Code here
+        int num = String.valueOf(Math.log(n)).length()+1;
+        
+        for(int i=n- num; i<n;i++){
+            
+            if(i + Integer.bitCount(i)==n){
+                return 0;
+            }
+        }
+      return 1;
+    }
+}
