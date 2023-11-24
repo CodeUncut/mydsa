43c02a25da914aef35ca1766ea17c486d5b53979
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-11-24 23:29:38 +0530

    Create gfgPotd_241123.java

---
+++ gfgPotd_241123.java
@@ -0,0 +1,20 @@
+class Solution {
+    ArrayList<Long> nthRowOfPascalTriangle(int n) {
+        // code here
+         ArrayList<Long> pre = new ArrayList<>();
+        long mod = 1000000007;
+        for (int i = 0; i < n; i++) {
+            ArrayList<Long> current = new ArrayList<>();
+            for (int j = 0; j < i + 1; j++) {
+                if (j == i | j == 0) {
+                    current.add(1l);
+                } else {
+                    long num = (pre.get(j) + pre.get(j - 1)) % mod;
+                    current.add(num);
+                }
+            }
+            pre = current;
+        }
+        return pre;
+    }
+}
