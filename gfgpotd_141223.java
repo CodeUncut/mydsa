c48ef8854eafde07d2add718c7cdb4b3e0504fa6
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-12-14 22:53:27 +0530

    Create gfgpotd_141223.java

---
+++ gfgpotd_141223.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    long countWays(int n,int k)
+    {
+        if(n == 1)
+            return k;
+            
+        long mod = (long) 1e9 + 7, same = 0, diff = k;
+        
+        for(int i = 2; i <= n; i++) 
+        {
+            long prev = same;
+            same = diff;
+            diff = ((prev + same) * (k - 1)) % mod;
+        }
+        
+        return (same + diff) % mod;
+    }
+}
