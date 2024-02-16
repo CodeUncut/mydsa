0b368d95f19900baa06a2182500b70888a94ee01
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2024-02-16 23:47:08 +0530

    Create gfgPotd_160224.java
    Going to indore tomorrow #newLife4

---
+++ gfgPotd_160224.java
@@ -0,0 +1,27 @@
+class Solution {
+    private Node prev;
+
+    private void inorder(Node curr) {
+        if (curr == null)
+            return;
+        inorder(curr.left);
+        prev.left = null;
+        prev.right = curr;
+        prev = curr;
+        inorder(curr.right);
+    }
+
+    public Node flattenBST(Node root) {
+        Node dummy = new Node(-1);
+        prev = dummy;
+
+        inorder(root);
+
+        prev.left = null;
+        prev.right = null;
+        Node ret = dummy.right;
+
+        // Delete dummy node (optional in Java due to automatic garbage collection)
+        return ret;
+    }
+}
