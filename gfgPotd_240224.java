23a996712ccd7218fb5d5b1dcc93ee42769f8ac9
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2024-02-24 22:50:48 +0530

    Create gfgPotd_240224.java
    Back from Mumbai with lot of lessons #luggage

---
+++ gfgPotd_240224.java
@@ -0,0 +1,20 @@
+class Solution
+{
+    public int maxSum(int n) 
+    { 
+        //code here
+       return sm(n);
+        
+    } 
+    public int sm(int n){
+        int sum=0;
+        if (n<2||n<3|| n<4 ){
+        
+        return (n);
+            
+        }
+        
+        sum+= (sm(n/2)+sm(n/3)+sm(n/4));
+        return Math.max(sum,n);
+    }
+}
